56ff22cee4a06eac65754e1e34ab757598a472ff
Nicolas Béhier-Dévigne <nbehierdevigne@gmail.com>
2015-06-05 20:57:02 +0200

    Initial project from aptoma/silex-bootstrap, yahoo/pure, voog/wysihtml

---
+++ web/index.php
@@ -0,0 +1,11 @@
+<?php
+$filename = __DIR__.preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
+if (php_sapi_name() === 'cli-server' && is_file($filename)) {
+    return false;
+}
+$app = require __DIR__ . '/../app/app.php';
+if ($app instanceof Silex\Application) {
+    $app->run();
+} else {
+    echo 'Failed to initialize application.';
+}
